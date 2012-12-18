a914059d32df1c5c829a4a768ff8edcbca4a74f8
James Sodini <jsodini@mac.com>
2012-12-18 10:11:41 -0800

    News feed layout

---
+++ SporkIt/SINewsFeedCell.h
@@ -0,0 +1,13 @@
+//
+//  SINewsFeedCell.h
+//  SporkIt
+//
+//  Created by James Sodini on 12/18/12.
+//  Copyright (c) 2012 SporkinIt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SINewsFeedCell : UITableViewCell
+
+@end
---
+++ SporkIt/SINewsFeedCell.m
@@ -0,0 +1,29 @@
+//
+//  SINewsFeedCell.m
+//  SporkIt
+//
+//  Created by James Sodini on 12/18/12.
+//  Copyright (c) 2012 SporkinIt. All rights reserved.
+//
+
+#import "SINewsFeedCell.h"
+
+@implementation SINewsFeedCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    
+    if (self) {
+        // Initialization code
+    }
+    
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
