4a486b5a1d130ef1792a9f0a55a3b7768ed12536
James Sodini <jsodini@mac.com>
2012-12-18 12:09:27 -0800

    Missing from the previous checkin

---
+++ SporkItTests/SINewsFeedDataTest.h
@@ -0,0 +1,13 @@
+//
+//  SINewsFeedDatatest.h
+//  SporkItTests
+//
+//  Created by James Sodini on 12/18/12.
+//  Copyright (c) 2012 SporkinIt. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SINewsFeedDataTest : SenTestCase
+
+@end
